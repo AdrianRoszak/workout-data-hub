25b255e8fd13403e02e6cbe5ed0dd90c2df5a030
Weirdo <adroszak@weirdo.codes>
2026-07-20 23:02:18 +0200

    feat: Import existing API Gateway v2 route and Lambda integration for Strava webhook

--- api_gateway.tf
+++ api_gateway.tf
@@ -3,3 +3,17 @@ resource "aws_apigatewayv2_api" "strava_webhook" {
   description ="HTTP API Gateway v2 endpoint that receives Strava webhook callback (activity create/update/delete) and proxies them to the stravaWebhookHandler Lambda."
   protocol_type = "HTTP"
 }
+
+resource "aws_apigatewayv2_route" "strava_webhook" {
+  api_id = aws_apigatewayv2_api.strava_webhook.id
+  route_key = "ANY /stravaWebhookHandler"
+  target = "integrations/${aws_apigatewayv2_integration.strava_webhook.id}"
+}
+
+resource "aws_apigatewayv2_integration" "strava_webhook" {
+  api_id = aws_apigatewayv2_api.strava_webhook.id
+  integration_type = "AWS_PROXY"
+  integration_uri = "arn:aws:lambda:eu-central-1:REDACTED_ACCOUNT_ID:function:stravaWebhookHandler"
+  integration_method = "POST"
+  payload_format_version = "2.0"
+}
