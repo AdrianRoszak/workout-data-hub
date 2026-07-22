18ecc9f0590599a01dc94e5da2658935823cc6b9
Weirdo <adroszak@weirdo.codes>
2026-07-22 22:12:15 +0200

    fix(lambda): increase timeout to 30s and memory to 256MB

--- modules/strava-webhook/main.tf
+++ modules/strava-webhook/main.tf
@@ -123,8 +123,8 @@ resource "aws_lambda_function" "strava_webhook" {
   role             = aws_iam_role.lambda_exec.arn
   filename         = "${path.module}/src/lambda_function.zip"
   source_code_hash = filebase64sha256("${path.module}/src/lambda_function.zip")
-  timeout          = 10
-  memory_size      = 128
+  timeout          = 30
+  memory_size      = 256
   architectures    = ["x86_64"]
 
   environment {
