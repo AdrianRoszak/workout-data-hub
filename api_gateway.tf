e3e866bbc8a0a7fc671e6607e12d23936753a4b6
Weirdo <adroszak@weirdo.codes>
2026-07-22 06:54:15 +0200

    Import API Gateway stage and Lambda permission for Strava webhook

--- api_gateway.tf
+++ api_gateway.tf
@@ -17,3 +17,10 @@ resource "aws_apigatewayv2_integration" "strava_webhook" {
   integration_method = "POST"
   payload_format_version = "2.0"
 }
+
+resource "aws_apigatewayv2_stage" "strava_webhook" {
+  api_id = aws_apigatewayv2_api.strava_webhook.id
+  name = "default"
+  auto_deploy = true
+  description = "Production stage for the Strava webhook endpoint. Auto-deploys on route/integration changes."
+}
